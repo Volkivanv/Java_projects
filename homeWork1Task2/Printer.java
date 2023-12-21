aa63d45447f4f8c028039c1807447a84a8d4e6b2
Ivan Volkov <volkivanv@mail.ru>
2023-12-22 00:02:11 +0300

    Сделана домашка к 1 семинару

---
+++ homeWork1Task2/Printer.java
@@ -0,0 +1,30 @@
+package homeWork1Task2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Answer {
+    public void printPrimeNums() {
+        // Напишите свое решение ниже
+        List<Integer> primes = new ArrayList<Integer>();
+        for (int i = 2; i < 1000; i++) {
+            boolean isPrime = true;
+            for (int j : primes) {
+                if (i % j == 0) isPrime = false;
+            }
+            if (isPrime) {
+                primes.add(i);
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class Printer {
+    public static void main(String[] args) {
+
+        Answer ans = new Answer();
+        ans.printPrimeNums();
+    }
+}
---
+++ homeWork1Task3/CalculatorPrint.java
@@ -0,0 +1,40 @@
+package homeWork1Task3;
+
+import java.text.DecimalFormat;
+
+class Calculator {
+    public String calculate(char op, int a, int b) {
+        // Введите свое решение ниже
+        switch (op) {
+            case '+': return new DecimalFormat("#0.0").format((float) a + (float) b);
+            case '-': return new DecimalFormat("#0.0").format((float) a - (float) b);
+            case '*': return new DecimalFormat("#0.0").format((float) a * (float) b);
+            case '/': return new DecimalFormat("#0.0").format((float) a / (float) b);
+            default: return "Некорректный оператор: '" + op + "'";
+        }
+    }
+}
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class CalculatorPrint{
+    public static void main(String[] args) {
+        int a = 0;
+        char op = ' ';
+        int b = 0;
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            a = 3;
+            op = '*';
+            b = 7;
+        } else {
+            a = Integer.parseInt(args[0]);
+            op = args[1].charAt(0);
+            b = Integer.parseInt(args[2]);
+        }
+
+        Calculator calculator = new Calculator();
+        String result = calculator.calculate(op, a, b);
+        System.out.println(result);
+    }
+}
